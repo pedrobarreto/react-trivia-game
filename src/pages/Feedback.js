8162ec4adf8deab6dc689e89cdda0e9721859acd
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-10-22 13:51:05 -0300

    Add requirement 14

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import { generateInfos } from '../utils/localStorage';
 
 class Feedback extends React.Component {
   render() {
+    const { score, assertions } = generateInfos();
     return (
       <div>
         <Header />
         <span data-testid="feedback-text">Voce foi horrivel</span>
+        <span data-testid="feedback-total-score">{score}</span>
+        <span data-testid="feedback-total-question">{assertions}</span>
       </div>
     );
   }
