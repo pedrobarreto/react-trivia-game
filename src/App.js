24d018d773a6e817daced4d39745e7f6ec350872
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-22 12:18:33 -0300

    Add requirement 11

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Config from './pages/Config';
+import Feedback from './pages/Feedback';
 import Game from './pages/Game';
 import Login from './pages/Login';
 
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" component={ Login } />
       <Route exact path="/game" component={ Game } />
       <Route exact path="/settings" component={ Config } />
+      <Route exact path="/feedback" component={ Feedback } />
     </Switch>
   );
 }
---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div>
+        <span data-testid="feedback-text">Voce foi horrivel</span>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
