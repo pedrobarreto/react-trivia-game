90fdf51e9994d3fd9b2005a3791534655f74ea42
Lucas Duarte <lucs.rkt@gmail.com>
2021-10-22 13:52:03 -0300

    Requisito 15 finalizado

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
@@ -7,6 +8,11 @@ class Feedback extends React.Component {
       <div>
         <Header />
         <span data-testid="feedback-text">Voce foi horrivel</span>
+        <Link to="/">
+          <button data-testid="btn-play-again" type="button">
+            Jogar novamente
+          </button>
+        </Link>
       </div>
     );
   }
