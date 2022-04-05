68fcaa481de593afdfd3071880193adc13e78803
Pedro <pedro@Pedros-MacBook-Pro.local>
2022-04-05 12:48:24 -0300

    fix gitignore

--- src/actions/types/index.js
+++
@@ -1,4 +0,0 @@
-export const LOGIN_INFO = 'LOGIN_INFO';
-export const SET_SCORE = 'SET_SCORE';
-export const SET_CONFIG = 'SET_CONFIG';
-export const RESET_CONFIG = 'RESET_CONFIG';
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import user from './user';
-import settings from './settings';
-
-const rootReducer = combineReducers({ user, settings });
-
-export default rootReducer;
